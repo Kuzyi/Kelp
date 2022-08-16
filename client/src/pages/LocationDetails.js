2169278571c3c56aa21eddc9fc8f603b4816ee5c
Cory <cjbenicak@gmail.com>
2022-08-16 16:23:26 -0400

    displayed details for individual locations

--- client/src/pages/LocationDetails.js
+++ client/src/pages/LocationDetails.js
@@ -1,9 +1,30 @@
 import { useParams } from 'react-router-dom'
+import Client from '../services/api'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const LocationDetails = () => {
   let { id } = useParams()
+  const [locationDetails, setLocationDetails] = useState('')
 
-  return <div></div>
+  useEffect(() => {
+    const getLocation = async () => {
+      const res = await axios.get(`http://localhost:3001/api/locations/${id}`)
+      setLocationDetails(res.data)
+      console.log(res.data)
+    }
+    getLocation()
+  }, [id])
+
+  return (
+    <div>
+      <h1>{locationDetails.beachName}</h1>
+      <h3>
+        {locationDetails.city} {locationDetails.state}
+      </h3>
+      <img src={locationDetails.image} />
+    </div>
+  )
 }
 
 export default LocationDetails
